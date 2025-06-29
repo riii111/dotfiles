bb2d8acf4074f82c95153aad863afa334e6b1b9f
riii111 <ryo.ichinose@nodex.inc>
2025-06-30 00:19:36 +0900

    update
    - remove unused plugins
    - fix: show file's category(tsx)
    - enable lspsaga

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -142,12 +142,6 @@ return {
     opts = {},
   },
 
-  -- UFO folding (disabled to prevent automatic folding)
-  {
-    "kevinhwang91/nvim-ufo",
-    dependencies = "kevinhwang91/promise-async",
-    enabled = false,
-  },
 
   -- Guess indent
   {
