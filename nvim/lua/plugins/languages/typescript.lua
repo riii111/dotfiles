750c4cd73155f3e942fe072438a372079e1fa0db
riii111 <ryo.ichinose@nodex.inc>
2025-12-01 16:40:03 +0900

    chore: lsp-actions supports tsx file

--- nvim/lua/plugins/languages/typescript.lua
+++ nvim/lua/plugins/languages/typescript.lua
@@ -48,6 +48,33 @@ return {
 
       vim.lsp.enable('ts_ls')
 
+      -- Keymaps (IntelliJ-like actions)
+      local lsp_actions_ok, lsp_actions = pcall(require, "utils.lsp-actions")
+      if lsp_actions_ok then
+        vim.api.nvim_create_autocmd("FileType", {
+          pattern = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+          callback = function()
+            local opts = { buffer = true, silent = true }
+            vim.keymap.set("n", "<M-CR>", lsp_actions.language_specific_code_action, opts)
+            vim.keymap.set("n", "<D-S-r>", lsp_actions.typescript_refactor_menu, opts)
+            vim.keymap.set("n", "<M-S-r>", lsp_actions.typescript_refactor_menu, opts)
+          end,
+        })
+      end
+
+      -- Format on save with null-ls
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = { "*.js", "*.jsx", "*.ts", "*.tsx", "*.json", "*.jsonc" },
+        callback = function()
+          vim.lsp.buf.format({
+            filter = function(client)
+              return client.name == "null-ls"
+            end,
+            timeout_ms = 5000,
+          })
+        end,
+      })
+
       -- Register biome sources (once, if project uses biome)
       local null_ls_ok, null_ls = pcall(require, "null-ls")
       if null_ls_ok and not vim.g._typescript_null_ls_registered then
