636071a7a95d601474d6350b0813ea7baa6fdc26
riii111 <ryo.ichinose@nodex.inc>
2025-12-14 18:59:12 +0900

    fix(nvim): prevent format loop with changedtick guard

--- nvim/lua/utils/format.lua
+++ nvim/lua/utils/format.lua
@@ -24,18 +24,26 @@ function M.async_format(bufnr)
   local formatter_name = M.config[vim.bo[bufnr].filetype]
   if not formatter_name then return end
 
+  -- Prevent re-entry loop
+  if vim.b[bufnr].async_format_running then return end
+
   local clients = vim.lsp.get_clients({ bufnr = bufnr, name = formatter_name })
   if #clients == 0 then return end
 
   local client = clients[1]
+  local changedtick = vim.b[bufnr].changedtick
+
+  vim.b[bufnr].async_format_running = true
   client.request("textDocument/formatting", vim.lsp.util.make_formatting_params({}), function(err, result)
-    if err or not result then return end
     vim.schedule(function()
-      if vim.api.nvim_buf_is_valid(bufnr) then
-        vim.lsp.util.apply_text_edits(result, bufnr, client.offset_encoding)
-        if vim.bo[bufnr].modified then
-          vim.api.nvim_buf_call(bufnr, function() vim.cmd("silent! update") end)
-        end
+      vim.b[bufnr].async_format_running = false
+      if err or not result then return end
+      -- Skip if buffer changed during format
+      if not vim.api.nvim_buf_is_valid(bufnr) or vim.b[bufnr].changedtick ~= changedtick then return end
+
+      vim.lsp.util.apply_text_edits(result, bufnr, client.offset_encoding)
+      if vim.bo[bufnr].modified then
+        vim.api.nvim_buf_call(bufnr, function() vim.cmd("silent! update") end)
       end
     end)
   end, bufnr)
